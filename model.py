f796cd3a69b9b5a7e8de27616aeb8fd707f01637
Jordana Tepper <jordana.t@wustl.edu>
2022-12-03 21:07:48 -0500

    py

--- model.py
+++ model.py
@@ -1,52 +1,13 @@
-import pandas as pd
-import numpy as np
-import string
-import re
+import streamlit as st
 
-import nltk
-from nltk.tokenize import regexp_tokenize, word_tokenize, RegexpTokenizer
-from nltk.corpus import stopwords, wordnet
-from nltk import pos_tag
-from nltk.stem import WordNetLemmatizer
-from nltk import FreqDist
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import confusion_matrix, recall_score
-from sklearn.metrics import plot_confusion_matrix
-from sklearn.metrics import classification_report
-from sklearn.metrics import plot_roc_curve
-from sklearn.metrics import roc_auc_score
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from imblearn.over_sampling import SMOTE
-from imblearn.pipeline import Pipeline, make_pipeline
+st.title('Simple Streamlit App')
 
-df = pd.read_csv('data/sentiment_tweets3.csv')
-df = df.drop(10313)
-df = df.drop(columns = ['Index'], axis = 1)
-df = df.rename(columns = {'message to examine':'tweet', 'label (depression result)': 'depression'})
-X = df['tweet']
-y = df['depression']
+st.text('Type a number in the box below')
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42)
-from nltk.stem.snowball import SnowballStemmer
-stemmer = SnowballStemmer(language="english")
-stopwords_list = stopwords.words('english')
+n = st.number_input('Number', step=1)
 
+st.write(f'{n} + 1 = {n+1}')
 
-from nltk.tokenize import RegexpTokenizer
-basic_token_pattern = r"(?u)\b\w\w+\b"
-tokenizer = RegexpTokenizer(basic_token_pattern)
-def stem_and_tokenize(document):
-    tokens = tokenizer.tokenize(document)
-    return [stemmer.stem(token) for token in tokens]
-stemmed_stopwords = [stemmer.stem(word) for word in stopwords_list]
-pipe_mnb_vec = Pipeline([
-  ('vect', CountVectorizer(stop_words=stemmed_stopwords, tokenizer=stem_and_tokenize)),
-   ('smote', SMOTE(random_state=42)),
-   ('mnb', MultinomialNB())
-])
-pipe_mnb_vec.fit(X_train, y_train)
-y_preds_mnb_vec = pipe_mnb_vec.predict(X_test)
-recall_score(y_test, y_preds_mnb_vec)
+s = st.text_input('Type a name in the box below')
 
+st.write(f'Hello {s}')
