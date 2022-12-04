fcf4eb31b1ab20aca7775c7e81a288dbde910725
Jordana Tepper <jordana.t@wustl.edu>
2022-12-03 21:39:37 -0500

    model

--- model.py
+++ model.py
@@ -1,8 +1,24 @@
 import streamlit as st
 import pickle
 
+
 with open('model3.pkl' , 'rb') as f:
     lr = pickle.load(f)
+
+from nltk.stem.snowball import SnowballStemmer
+stemmer = SnowballStemmer(language="english")
+stopwords_list = stopwords.words('english')
+
+
+from nltk.tokenize import RegexpTokenizer
+basic_token_pattern = r"(?u)\b\w\w+\b"
+tokenizer = RegexpTokenizer(basic_token_pattern)
+
+def stem_and_tokenize(document):
+    tokens = tokenizer.tokenize(document)
+    return [stemmer.stem(token) for token in tokens]
+
+stemmed_stopwords = [stemmer.stem(word) for word in stopwords_list]
 
 st.title('Depression in Tweets Detector')
 
