9965954c87afdc5fbf4e0ec3eca53a9feb0c9fd6
Jordana Tepper <jordana.t@wustl.edu>
2022-12-03 21:10:07 -0500

    app

--- model.py
+++ model.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
-st.title('Simple Streamlit App')
+st.title('Depression in Tweets Detector')
 
-st.text('Type a number in the box below')
+st.text('Type a tweet')
 
 n = st.number_input('Number', step=1)
 
+st.write('Possibility of Depression?')
 st.write(f'{n} + 1 = {n+1}')
 
 s = st.text_input('Type a name in the box below')
